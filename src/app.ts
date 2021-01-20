5b62c7f33433dd16268918e330305524fe0b4eca
TomRaaff <tom.raaff@jcore.com>
2021-01-20 20:25:39 +0100

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+/*
+	start writing your app!
+ */
+export default function start() {
+	console.log('Lets play tic tac toe!');
+}
+
+start();
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/app.ts',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	devtool: 'inline-source-map',
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.ts', '.js'],
+	},
+};
